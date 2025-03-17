a2727912613e81caaca78b3506b3655d41fd0a92
Jesus Jackson <jesus@hydeandassociates.com>
2025-03-17 11:01:21 +0200

    Adding comment about the problem with ts-jest

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,5 +1,9 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
-
+/*
+ * The plan was to have all the tests written in
+ * typescript, but I don't have time to figure out
+ * why ts-jest is not doing its job right now.
+ */
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
